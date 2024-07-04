3b9f5d2c7cffcb8efe47b946f58db649be6fe1f1
thealkalineknight <139799163+thealkalineknight@users.noreply.github.com>
2024-07-03 20:12:31 -0400

    Update cube.cpp

--- v4biggerFormulas/cube.cpp
+++ v4biggerFormulas/cube.cpp
@@ -5,58 +5,68 @@ Cube::Cube() {
 }
 
 void Cube::Update() {
-    Renderer();
 
     th += 0.01;
-    vector<Vector3> mRx = {
-        Vector3{1, 0, 0},
-        Vector3{0, cos(th), sin(th)},
-        Vector3{0, -sin(th), cos(th)} };
 
-    vector<Vector3> mRy = {
-        Vector3{cos(th), 0, -sin(th)},
-        Vector3{0, 1, 0},
-        Vector3{sin(th), 0, cos(th)} };
+    vector<vector<float>> tRx = {
+        vector<float>{1, 0, 0, 0},
+        vector<float>{0, cos(th), sin(th), 0},
+        vector<float>{0, -sin(th), cos(th), 0},
+        vector<float>{0, 0, 0, 1} };
 
-    vector<Vector3> mRz = {
-        Vector3{cos(th), sin(th), 0},
-        Vector3{-sin(th), cos(th), 0},
-        Vector3{0, 0, 1} };
+    vector<vector<float>> tRy = {
+        vector<float>{cos(th), 0, -sin(th), 0},
+        vector<float>{0, 1, 0, 0},
+        vector<float>{sin(th), 0, cos(th), 0},
+        vector<float>{0, 0, 0, 1} };
 
-    for (int i = 0; i < matrixA.size(); i++) {
-        Vector3 p = matrixA[i];
-        float RxPx = p.x * mRx[0].x + p.y * mRx[1].x + p.z * mRx[2].x;
-        float RxPy = p.x * mRx[0].y + p.y * mRx[1].y + p.z * mRx[2].y;
-        float RxPz = p.x * mRx[0].z + p.y * mRx[1].z + p.z * mRx[2].z;
+    vector<vector<float>> tRz = {
+        vector<float>{cos(th), sin(th), 0, 0},
+        vector<float>{-sin(th), cos(th), 0, 0},
+        vector<float>{0, 0, 1, 0},
+        vector<float>{0, 0, 0, 1} };
 
-        float RyPx = RxPx * mRy[0].x + RxPy * mRy[1].x + RxPz * mRy[2].x;
-        float RyPy = RxPx * mRy[0].y + RxPy * mRy[1].y + RxPz * mRy[2].y;
-        float RyPz = RxPx * mRy[0].z + RxPy * mRy[1].z + RxPz * mRy[2].z;
 
-        float RzPx = RyPx * mRz[0].x + RyPy * mRz[1].x + RyPz * mRz[2].x;
-        float RzPy = RyPx * mRz[0].y + RyPy * mRz[1].y + RyPz * mRz[2].y;
-        float RzPz = RyPx * mRz[0].z + RyPy * mRz[1].z + RyPz * mRz[2].z;
+    for (int i = 0; i < matrixB.size(); i++) {
+        vector<int> p = matrixB[i];
 
-        float z = 1 / (dist - RzPz);
+        float RxPx = p[0] * tRx[0][0] + p[1] * tRx[1][0] + p[2] * tRx[2][0] + p[3] * tRx[3][0];
+        float RxPy = p[0] * tRx[0][1] + p[1] * tRx[1][1] + p[2] * tRx[2][1] + p[3] * tRx[3][1];
+        float RxPz = p[0] * tRx[0][2] + p[1] * tRx[1][2] + p[2] * tRx[2][2] + p[3] * tRx[3][2];
+        float RxPt = p[0] * tRx[0][3] + p[1] * tRx[1][3] + p[2] * tRx[2][3] + p[3] * tRx[3][3];
 
-        vector<Vector3> pjM = {
-            Vector3{z, 0, 0},
-            Vector3{0, z, 0}
-        };
+        float RyPx = RxPx * tRy[0][0] + RxPy * tRy[1][0] + RxPz * tRy[2][0] + RxPt * tRy[3][0];
+        float RyPy = RxPx * tRy[0][1] + RxPy * tRy[1][1] + RxPz * tRy[2][1] + RxPt * tRy[3][1];
+        float RyPz = RxPx * tRy[0][2] + RxPy * tRy[1][2] + RxPz * tRy[2][2] + RxPt * tRy[3][2];
+        float RyPt = RxPx * tRy[0][3] + RxPy * tRy[1][3] + RxPz * tRy[2][3] + RxPt * tRy[3][3];
 
-        float pX = RzPx * pjM[0].x + RzPy * pjM[1].x;
-        float pY = RzPx * pjM[0].y + RzPy * pjM[1].y;
+        float RzPx = RyPx * tRz[0][0] + RyPy * tRz[1][0] + RyPz * tRz[2][0] + RyPt * tRz[3][0];
+        float RzPy = RyPx * tRz[0][1] + RyPy * tRz[1][1] + RyPz * tRz[2][1] + RyPt * tRz[3][1];
+        float RzPz = RyPx * tRz[0][2] + RyPy * tRz[1][2] + RyPz * tRz[2][2] + RyPt * tRz[3][2];
+        float RzPt = RyPx * tRz[0][3] + RyPy * tRz[1][3] + RyPz * tRz[2][3] + RyPt * tRz[3][3];
 
-        pX = int(pX * st.scale + st.scrWid / 2);
-        pY = int(pY * st.scale + st.scrHei / 2);
+        RzPz = maxDist - RzPz;
 
-        DrawCircle(pX, pY, pt.radius, WHITE);
+        float Px = RzPx * pM[0][0] + RzPy * pM[1][0] + RzPz * pM[2][0] + RzPt * pM[3][0];
+        float Py = RzPx * pM[0][1] + RzPy * pM[1][1] + RzPz * pM[2][1] + RzPt * pM[3][1];
+        float Pz = RzPx * pM[0][2] + RzPy * pM[1][2] + RzPz * pM[2][2] + RzPt * pM[3][2];
+        float Pt = RzPx * pM[0][3] + RzPy * pM[1][3] + RzPz * pM[2][3] + RzPt * pM[3][3];
 
-        matrixAt[i].x = pX;
-        matrixAt[i].y = pY;
+        if (Pt != 0) {
+            Px /= Pt; Py /= Pt; Pz /= Pt;
+        }
+
+        Px = (Px * st.scale + st.scrWid / 2);
+        Py = (Py * st.scale + st.scrHei / 2);
+
+        DrawCircle(Px, Py, pt.radius, BLACK);
+
+        matrixAt[i].x = Px;
+        matrixAt[i].y = Py;
     }
     ConnectLine();
 }
+
 
 void Cube::ConnectLine() {
     for (int i = 0; i < 4; i++) {
@@ -67,48 +77,5 @@ void Cube::ConnectLine() {
 }
 
 void Cube::Connect(int i, int j) {
-    DrawLine(matrixAt[i].x, matrixAt[i].y, matrixAt[j].x, matrixAt[j].y, WHITE);
-}
-
-void Cube::Renderer() {
-    float fov = PI / 2;
-    float zNear = 0.1;
-    float zFar = 1000;
-
-    float a = st.scrWid / st.scrHei;
-    float F = 1 / tan(fov / 2);
-
-    float aF = a * F;
-    float q = zFar / (zFar - zNear);
-    float Znq = zNear * q;
-
-    vector<vector<float>> pM = {
-        vector<float>{aF, 0, 0, 0},
-        vector<float>{0, F, 0, 0},
-        vector<float>{0, 0, q, 1},
-        vector<float>{0, 0, -Znq, 0} };
-
-    for (int i = 0; i < matrixB.size(); i++) {
-        vector<int> p = matrixB[i];
-        float Px = p[0] * pM[0][0] + p[1] * pM[1][0] + p[2] * pM[2][0] + p[3] * pM[3][0];
-        float Py = p[0] * pM[0][1] + p[1] * pM[1][1] + p[2] * pM[2][1] + p[3] * pM[3][1];
-        float Pz = p[0] * pM[0][2] + p[1] * pM[1][2] + p[2] * pM[2][2] + p[3] * pM[3][2];
-        float Pt = p[0] * pM[0][3] + p[1] * pM[1][3] + p[2] * pM[2][3] + p[3] * pM[3][3];
-
-        if (Pt != 0) {
-            Px /= Pt; Py /= Pt; Pz /= Pt;
-        }
-
-        matrixB[0][2] += 3; matrixB[1][2] += 3; matrixB[2][2] += 3;
-
-        matrixB[0][0] += 1; matrixB[0][1] += 1;
-        matrixB[1][0] += 1; matrixB[1][1] += 1;
-        matrixB[2][0] += 1; matrixB[2][1] += 1;
-
-        matrixB[0][0] *= st.scrWid / 2; matrixB[0][1] *= st.scrHei / 2;
-        matrixB[1][0] *= st.scrWid / 2; matrixB[1][1] *= st.scrHei / 2;
-        matrixB[2][0] *= st.scrWid / 2; matrixB[2][1] *= st.scrHei / 2;
-
-        DrawCircle(Px, Py, pt.radius, BLACK);
-    }
+    DrawLine(matrixAt[i].x, matrixAt[i].y, matrixAt[j].x, matrixAt[j].y, BLACK);
 }
