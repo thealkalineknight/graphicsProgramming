dcebae8c8d96e6f2eb03e0947cc78a5868e83e4b
thealkalineknight <139799163+thealkalineknight@users.noreply.github.com>
2024-07-03 20:12:18 -0400

    Update cube.h

--- v4biggerFormulas/cube.h
+++ v4biggerFormulas/cube.h
@@ -11,34 +11,28 @@ public:
     void Update();
     void ConnectLine();
     void Connect(int i, int j);
-    void Renderer();
-    // void UpdateRots();
-    // void CalcRots();
-    // void ProjPoints(float rzpx, float rzpy, float rzpz);
-    // void Draw(float x, float y);
 
-    float dist = 3;
-
-    float Tf = -1.3;
-
-    float Sx = 1;
-    float Sy = 1;
-    float Sz = 1;
-    float Sf = 1.9;
-
+    float maxDist = 3;
     float th = 0;
 
 private:
-    vector<Vector3> matrixA = {
-        Vector3{-1, -1, 1},
-        Vector3{1, -1, 1},
-        Vector3{1, 1, 1}, 
-        Vector3{-1, 1, 1}, 
-    
-        Vector3{-1, -1, -1}, 
-        Vector3{1, -1, -1}, 
-        Vector3{1, 1, -1}, 
-        Vector3{-1, 1, -1} };
+
+    float fov = PI / 2;
+    float zNear = 0.1;
+    float zFar = 1000;
+
+    float a = st.scrWid / st.scrHei;
+    float F = 1 / tan(fov / 2);
+
+    float aF = a * F;
+    float q = zFar / (zFar - zNear);
+    float Znq = zNear * q;
+
+    vector<vector<float>> pM = {
+        vector<float>{aF, 0, 0, 0},
+        vector<float>{0, F, 0, 0},
+        vector<float>{0, 0, q, 1},
+        vector<float>{0, 0, -Znq, 0} };
 
     vector<vector<int>> matrixB = {
         vector<int>{-1, -1, 1, 1},
@@ -61,28 +55,4 @@ private:
         Vector3{0, 0, 0},
         Vector3{0, 0, 0},
         Vector3{0, 0, 0} };
-
-    //----------
-
-    vector<Vector3> mS = {
-        Vector3{Sx, 0, 0},
-        Vector3{0, Sy, 0},
-        Vector3{0, 0, Sz} };
-
-    //----------
-
-    vector<Vector3> mRx = {
-        Vector3{1, 0, 0},
-        Vector3{0, cos(th), sin(th)},
-        Vector3{0, -sin(th), cos(th)} };
-
-    vector<Vector3> mRy = {
-        Vector3{cos(th), 0, -sin(th)},
-        Vector3{0, 1, 0},
-        Vector3{sin(th), 0, cos(th)} };
-
-    vector<Vector3> mRz = {
-        Vector3{cos(th), sin(th), 0},
-        Vector3{-sin(th), cos(th), 0},
-        Vector3{0, 0, 1} };
 };
