fe84d76416e3e3ec56ef7bc9c1fe7b96a6ab6702
thealkalineknight <139799163+thealkalineknight@users.noreply.github.com>
2024-07-12 16:24:16 -0400

    Update cube.cpp

--- v5manipulatingViewProperties/cube.cpp
+++ v5manipulatingViewProperties/cube.cpp
@@ -6,63 +6,52 @@ Cube::Cube() {
 
 void Cube::Update() {
 
+    //th += 0.01;
     KeyPress();
 
     vector<vector<float>> tRx = {
         vector<float>{1, 0, 0, 0},
-        vector<float>{0, cos(th), sin(th), 0},
-        vector<float>{0, -sin(th), cos(th), 0},
+        vector<float>{0, cos(thX), sin(thX), 0},
+        vector<float>{0, -sin(thX), cos(thX), 0},
         vector<float>{0, 0, 0, 1} };
 
     vector<vector<float>> tRy = {
-        vector<float>{cos(th), 0, -sin(th), 0},
+        vector<float>{cos(thY), 0, -sin(thY), 0},
         vector<float>{0, 1, 0, 0},
-        vector<float>{sin(th), 0, cos(th), 0},
+        vector<float>{sin(thY), 0, cos(thY), 0},
         vector<float>{0, 0, 0, 1} };
 
     vector<vector<float>> tRz = {
-        vector<float>{cos(th), sin(th), 0, 0},
-        vector<float>{-sin(th), cos(th), 0, 0},
+        vector<float>{cos(thZ), sin(thZ), 0, 0},
+        vector<float>{-sin(thZ), cos(thZ), 0, 0},
         vector<float>{0, 0, 1, 0},
         vector<float>{0, 0, 0, 1} };
+
 
     for (int i = 0; i < matrixB.size(); i++) {
         vector<int> p = matrixB[i];
 
-        if (dir == 1) {
-            Rx = p[0] * tRx[0][0] + p[1] * tRx[1][0] + p[2] * tRx[2][0] + p[3] * tRx[3][0];
-            Ry = p[0] * tRx[0][1] + p[1] * tRx[1][1] + p[2] * tRx[2][1] + p[3] * tRx[3][1];
-            Rz = p[0] * tRx[0][2] + p[1] * tRx[1][2] + p[2] * tRx[2][2] + p[3] * tRx[3][2];
-            Rt = p[0] * tRx[0][3] + p[1] * tRx[1][3] + p[2] * tRx[2][3] + p[3] * tRx[3][3];
-        }
+        float RxPx = p[0] * tRx[0][0] + p[1] * tRx[1][0] + p[2] * tRx[2][0] + p[3] * tRx[3][0];
+        float RxPy = p[0] * tRx[0][1] + p[1] * tRx[1][1] + p[2] * tRx[2][1] + p[3] * tRx[3][1];
+        float RxPz = p[0] * tRx[0][2] + p[1] * tRx[1][2] + p[2] * tRx[2][2] + p[3] * tRx[3][2];
+        float RxPt = p[0] * tRx[0][3] + p[1] * tRx[1][3] + p[2] * tRx[2][3] + p[3] * tRx[3][3];
 
-        if (dir == 2) {
-            Rx = p[0] * tRy[0][0] + p[1] * tRy[1][0] + p[2] * tRy[2][0] + p[3] * tRy[3][0];
-            Ry = p[0] * tRy[0][1] + p[1] * tRy[1][1] + p[2] * tRy[2][1] + p[3] * tRy[3][1];
-            Rz = p[0] * tRy[0][2] + p[1] * tRy[1][2] + p[2] * tRy[2][2] + p[3] * tRy[3][2];
-            Rt = p[0] * tRy[0][3] + p[1] * tRy[1][3] + p[2] * tRy[2][3] + p[3] * tRy[3][3];
-        }
+        float RyPx = RxPx * tRy[0][0] + RxPy * tRy[1][0] + RxPz * tRy[2][0] + RxPt * tRy[3][0];
+        float RyPy = RxPx * tRy[0][1] + RxPy * tRy[1][1] + RxPz * tRy[2][1] + RxPt * tRy[3][1];
+        float RyPz = RxPx * tRy[0][2] + RxPy * tRy[1][2] + RxPz * tRy[2][2] + RxPt * tRy[3][2];
+        float RyPt = RxPx * tRy[0][3] + RxPy * tRy[1][3] + RxPz * tRy[2][3] + RxPt * tRy[3][3];
 
-        if (dir == 3) {
-            Rx = p[0] * tRz[0][0] + p[1] * tRz[1][0] + p[2] * tRz[2][0] + p[3] * tRz[3][0];
-            Ry = p[0] * tRz[0][1] + p[1] * tRz[1][1] + p[2] * tRz[2][1] + p[3] * tRz[3][1];
-            Rz = p[0] * tRz[0][2] + p[1] * tRz[1][2] + p[2] * tRz[2][2] + p[3] * tRz[3][2];
-            Rt = p[0] * tRz[0][3] + p[1] * tRz[1][3] + p[2] * tRz[2][3] + p[3] * tRz[3][3];
-        }
+        float RzPx = RyPx * tRz[0][0] + RyPy * tRz[1][0] + RyPz * tRz[2][0] + RyPt * tRz[3][0];
+        float RzPy = RyPx * tRz[0][1] + RyPy * tRz[1][1] + RyPz * tRz[2][1] + RyPt * tRz[3][1];
+        float RzPz = RyPx * tRz[0][2] + RyPy * tRz[1][2] + RyPz * tRz[2][2] + RyPt * tRz[3][2];
+        float RzPt = RyPx * tRz[0][3] + RyPy * tRz[1][3] + RyPz * tRz[2][3] + RyPt * tRz[3][3];
 
-        if (dir == 0) {
-            Rx = p[0];
-            Ry = p[1];
-            Rz = p[2];
-            Rt = p[3];
-        }
+        RzPz = maxDist - RzPz;
 
-        Rz = maxDist - Rz;
-
-        float Px = Rx * pM[0][0] + Ry * pM[1][0] + Rz * pM[2][0] + Rt * pM[3][0];
-        float Py = Rx * pM[0][1] + Ry * pM[1][1] + Rz * pM[2][1] + Rt * pM[3][1];
-        float Pz = Rx * pM[0][2] + Ry * pM[1][2] + Rz * pM[2][2] + Rt * pM[3][2];
-        float Pt = Rx * pM[0][3] + Ry * pM[1][3] + Rz * pM[2][3] + Rt * pM[3][3];
+        float Px = RzPx * pM[0][0] + RzPy * pM[1][0] + RzPz * pM[2][0] + RzPt * pM[3][0];
+        float Py = RzPx * pM[0][1] + RzPy * pM[1][1] + RzPz * pM[2][1] + RzPt * pM[3][1];
+        float Pz = RzPx * pM[0][2] + RzPy * pM[1][2] + RzPz * pM[2][2] + RzPt * pM[3][2];
+        float Pt = RzPx * pM[0][3] + RzPy * pM[1][3] + RzPz * pM[2][3] + RzPt * pM[3][3];
 
         if (Pt != 0) {
             Px /= Pt; Py /= Pt; Pz /= Pt;
@@ -70,7 +59,7 @@ void Cube::Update() {
 
         Px = Px / -Pz;
         Py = Py / -Pz;
-            
+
         Px = (Px * st.scale + st.scrWid / 2);
         Py = (Py * st.scale + st.scrHei / 2);
 
@@ -83,30 +72,16 @@ void Cube::Update() {
 }
 
 void Cube::KeyPress() {
-    if (IsKeyDown(KEY_A)) {
-        th += 0.1;
-        dir = 2;
-    }
-    if (IsKeyDown(KEY_D)) {
-        th -= 0.1;
-        dir = 2;
-    }
-    if (IsKeyDown(KEY_W)) {
-        th += 0.1;
-        dir = 1;
-    }
-    if (IsKeyDown(KEY_S)) {
-        th -= 0.1;
-        dir = 1;
-    }
-    if (IsKeyDown(KEY_Q)) {
-        th += 0.1;
-        dir = 3;
-    }
-    if (IsKeyDown(KEY_Z)) {
-        th -= 0.1;
-        dir = 3;
-    }
+    if (IsKeyDown(KEY_A)) thX += 0.1;
+    if (IsKeyDown(KEY_D)) thX -= 0.1;
+
+    if (IsKeyDown(KEY_W)) thY += 0.1;
+    if (IsKeyDown(KEY_S)) thY -= 0.1;
+
+    if (IsKeyDown(KEY_Q)) thZ += 0.1;
+    if (IsKeyDown(KEY_Z)) thZ -= 0.1;
+
+    if (IsKeyPressed(KEY_R)) thX = thY = thZ = 0;
 }
 
 void Cube::ConnectLine() {
