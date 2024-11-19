afb6e6ffdc3f95b7fd1253424612ab32fc4bf717
thealkalineknight <139799163+thealkalineknight@users.noreply.github.com>
2024-11-18 21:59:45 -0500

    Add files via upload

--- v6torusTrouble/donut.cpp
+++ v6torusTrouble/donut.cpp
@@ -1,39 +1,43 @@
 #include "donut.h"
 
 Donut::Donut() {
+    arrCTh.reserve(thSize);
+    arrSTh.reserve(thSize);
+    arrCPh.reserve(phSize);
+    arrSPh.reserve(phSize);
     calcTrig();
 }
 
 void Donut::calcTrig() {
     for (float th = 0; th < 2 * PI; th += thspc) {
-        cosThValues.push_back(cos(th));
-        sinThValues.push_back(sin(th));
+        arrCTh.push_back(cos(th));
+        arrSTh.push_back(sin(th));
     }
     for (float ph = 0; ph < 2 * PI; ph += phspc) {
-        cosPhValues.push_back(cos(ph));
-        sinPhValues.push_back(sin(ph));
+        arrCPh.push_back(cos(ph));
+        arrSPh.push_back(sin(ph));
     }
 }
 
 void Donut::Update() {
 
-    al += 0.04; // *st.delta;
-    be += 0.02; // *st.delta;
+    al += 0.04f; // *st.delta;
+    be += 0.02f; // *st.delta;
 
     cAl = cos(al);
     sAl = sin(al);
     cBe = cos(be);
     sBe = sin(be);
 
-    for (size_t i = 0; i < cosThValues.size(); ++i) {
-        float cTh = cosThValues[i], sTh = sinThValues[i];
+    for (size_t i = 0; i < arrCTh.size(); ++i) {
+        cTh = arrCTh[i], sTh = arrSTh[i];
 
-        float crX = midR + cirR * cTh;
-        float crY = cirR * sTh;
+        crX = midR + cirR * cTh;
+        crY = cirR * sTh;
 
-        for (size_t j = 0; j < cosPhValues.size(); ++j) {
-            float cPh = cosPhValues[j];
-            float sPh = sinPhValues[j];
+        for (size_t j = 0; j < arrCPh.size(); ++j) {
+            cPh = arrCPh[j];
+            sPh = arrSPh[j];
 
             float mRy[3][3] = {
                 {cPh, 0, -sPh},
@@ -53,15 +57,15 @@ void Donut::Update() {
                 {0, 0, 1} };
             MatMult(RxPx, RxPy, RxPz, mRz, RzPx, RzPy, RzPz);
 
-            float z = 1 / (TorCamDist - RzPz);  // oldDist - z
+            z = 1 / (TorCamDist - RzPz);  // oldDist - z
 
             float pjM[2][3] = {
                 {z, 0, 0},
                 {0, z, 0}
             };
 
-            float pX = RzPx * pjM[0][0] + RzPy * pjM[1][0];
-            float pY = RzPx * pjM[0][1] + RzPy * pjM[1][1];
+            pX = RzPx * pjM[0][0] + RzPy * pjM[1][0];
+            pY = RzPx * pjM[0][1] + RzPy * pjM[1][1];
 
             pX = int(pX * st.scale + st.scrWid / 2.0);
             pY = int(pY * st.scale + st.scrHei / 2.0);
@@ -76,35 +80,3 @@ void Donut::MatMult(float mx, float my, float mz, float mat[3][3], float& outX, 
     outY = mx * mat[0][1] + my * mat[1][1] + mz * mat[2][1];
     outZ = mx * mat[0][2] + my * mat[1][2] + mz * mat[2][2];
 }
-
-/*
-#include <Eigen/Dense>
-using namespace Eigen;
-
-int main() {
-    MatrixXd mat1(2, 3); // 2x3 matrix
-    MatrixXd mat2(3, 2); // 3x2 matrix
-    mat1 << 1, 2, 3, 4, 5, 6;
-    mat2 << 7, 8, 9, 10, 11, 12;
-    MatrixXd result = mat1 * mat2;
-    std::cout << "Result:\n" << result << std::endl;
-    return 0;
-}
-
-========================
-
-#include <armadillo>
-using namespace arma;
-
-int main() {
-    mat mat1 = {{1, 2, 3}, {4, 5, 6}}; // 2x3 matrix
-    mat mat2 = {{7, 8}, {9, 10}, {11, 12}}; // 3x2 matrix
-    mat result = mat1 * mat2;
-    result.print("Result:");
-    return 0;
-}
-
-=========================
-
-
-*/
--- v6torusTrouble/donut.h
+++ v6torusTrouble/donut.h
@@ -4,34 +4,43 @@
 class Donut
 {
 private:
-    vector<float> cosThValues;
-    vector<float> sinThValues;
-    vector<float> cosPhValues;
-    vector<float> sinPhValues;
+    static constexpr float thspc = 0.07f;
+    static constexpr float phspc = 0.07f;
 
-    float cAl;
-    float sAl;
-    float cBe;
-    float sBe;
+    static constexpr size_t thSize = static_cast<size_t>(2 * PI / thspc);
+    static constexpr size_t phSize = static_cast<size_t>(2 * PI / phspc);
 
-    const int cirR = 1; // donut cross circ radius Z
-    const int midR = 2; // donut radius
-    const int TorCamDist = 5; // distance from donut to camera
-    const int torR = cirR + midR; // custom var
+    vector<float> arrCTh; 
+    vector<float> arrSTh;
+    vector<float> arrCPh;
+    vector<float> arrSPh;
 
-    const float thspc = 0.07;
-    const float phspc = 0.02;
+    float al, be = 0.0;
+
+    float cAl, sAl;
+    float cBe, sBe;
+
+    float cTh, sTh;
+
+    static constexpr int cirR = 1; // donut cross circ radius Z
+    static constexpr int midR = 2; // donut radius
+    static constexpr int TorCamDist = 5; // distance from donut to camera
+    static constexpr int torR = cirR + midR; // custom var
+
+    float crX, crY;
+
+    float cPh, sPh;
+
+    static constexpr float dist = 3.0f;  // trad
 
     float RyPx, RyPy, RyPz;
-
-    float al = 0; float be = 0;
-
     float RxPx, RxPy, RxPz;
-
     float RzPx, RzPy, RzPz;
 
-    // old
-    float dist = 3;
+    float z;
+
+    float pX, pY;
+
 
 public:
     Settings st;
